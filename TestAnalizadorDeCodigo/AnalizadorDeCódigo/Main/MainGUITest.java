ba32e6587cab81ccead19fca20db9432d0da989e
Tapiiaa <ptapilob@myuax.com>
2024-05-10 14:07:31 +0200

    test de la app de analizador de código

---
+++ TestAnalizadorDeCodigo/AnalizadorDeCódigo/Main/MainGUITest.java
@@ -0,0 +1,12 @@
+package AnalizadorDeCódigo.Main;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainGUITest {
+
+    @Test
+    void main() {
+    }
+}
